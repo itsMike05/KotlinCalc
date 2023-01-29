901bb163aa76b99a098fa04953dbb4802b7ac498
Michał Pelc <michal.pelc@live.com>
2023-01-29 23:40:30 +0100

    Added CalcVM (View Model)

--- app/src/main/java/com/itsmike/kotlincalc/CalcState.kt
+++ app/src/main/java/com/itsmike/kotlincalc/CalcState.kt
@@ -1,7 +1,7 @@
 package com.itsmike.kotlincalc
 
 data class CalcState(
-    val number1: String,
-    val number2: String,
+    val number1: String = "",
+    val number2: String = "",
     val operation: CalcOperation? = null
 )
